e12e013f99078cffac7051c57ac8a124e7adc97d
imtiaz <programmer.imtiaz@gmail.com>
2018-01-04 08:20:07 +0600

    CREATE- EidBonus class created. ADD- EidBonus, setEidBonusWithSalary, getBonusWithSalary methods added

---
+++ app/src/main/java/com/example/dipto/dagger2practice/EidBonus.java
@@ -0,0 +1,27 @@
+package com.example.dipto.dagger2practice;
+
+/**
+ * Created by Dipto on 1/4/2018.
+ */
+
+public class EidBonus {
+
+    private int bonusAmount ;
+
+    public EidBonus(){
+        this.bonusAmount = 5000 ;
+    }
+
+    public int getSalaryWithEidBonus() {
+        return bonusAmount;
+    }
+
+    public void setBonusWithSalary(int salaryAmount) {
+        this.bonusAmount = this.bonusAmount + salaryAmount ;
+    }
+
+    public void withoutBonus(){
+        this.bonusAmount = 0 ;
+    }
+
+}
