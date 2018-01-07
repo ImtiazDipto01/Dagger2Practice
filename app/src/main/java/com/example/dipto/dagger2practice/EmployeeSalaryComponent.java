bead310e0dca4a62d465fd9094bdd3c6ff0f2594
imtiaz <programmer.imtiaz@gmail.com>
2018-01-07 08:30:06 +0600

    CREATE- EmployeeSalaryComponent interface ADD- added module connection to EmployeeSalaryModule class

---
+++ app/src/main/java/com/example/dipto/dagger2practice/EmployeeSalaryComponent.java
@@ -0,0 +1,16 @@
+package com.example.dipto.dagger2practice;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by Dipto on 1/7/2018.
+ */
+
+@Singleton
+@Component(modules = {EmployeeSalaryModule.class})
+public interface EmployeeSalaryComponent {
+
+    EmployeeSalary provideEmployeeSalary();
+}
