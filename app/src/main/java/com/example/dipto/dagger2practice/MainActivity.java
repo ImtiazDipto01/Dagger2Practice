ebc3f36a6bec0fb0c4009a39d371b684f86183c0
imtiaz <programmer.imtiaz@gmail.com>
2018-01-08 08:13:18 +0600

    ADD- added Butterknife library and added button click activity change in MainActivity

--- app/src/main/java/com/example/dipto/dagger2practice/MainActivity.java
+++ app/src/main/java/com/example/dipto/dagger2practice/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.dipto.dagger2practice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.injectBtn)
+    Button injectBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.injectBtn)
+    public void onInjectClicked() {
+
+        Intent intent = new Intent(MainActivity.this, InjectActivity.class) ;
+        startActivity(intent);
+
     }
 }
